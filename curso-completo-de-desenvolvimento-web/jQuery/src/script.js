1f2b60cf76c396f25e004d58b439f0d3d09c0c3a
Artur Bani <arturbani@gmail.com>
2018-12-10 10:39:14 -0200

    tictactoe done

--- curso-completo-de-desenvolvimento-web/jQuery/src/script.js
+++ curso-completo-de-desenvolvimento-web/jQuery/src/script.js
@@ -1,5 +1,12 @@
 var rodada = 1;
 
+// array multidimensional para guardar os dados do jogo
+var matrizJogo = Array(3);
+matrizJogo['a'] = Array(3);
+matrizJogo['b'] = Array(3);
+matrizJogo['c'] = Array(3);
+
+
 $(document).ready(function (){
     $('#btn-start').click( function (){ 
         // checagem dos nomes dos jogadores
@@ -29,6 +36,8 @@ $(document).ready(function (){
         $('#main-page').hide();
         $('#tic-tac-toe').show();
 
+        iniciaMatriz();
+
     });
 
     $('.quadrado').click( function (){ 
@@ -40,6 +49,8 @@ $(document).ready(function (){
     function jogada(id){
         var icon = '';
         var player = 0;
+        // array que stora a linha na posição 0 e a coluna na posição 1
+        var linha_coluna = id.split('-');
 
         // checa qual jogador clicou através da variavel rodada
         // rodada % 2, se resta 1 é ímpar, jogador 1
@@ -58,8 +69,100 @@ $(document).ready(function (){
 
         rodada++;
 
+        // coloca a marcação da jogada na grade
         $('#'+id).css('background-image', icon);
+
+        // coloca o dado na posição correta do array
+		matrizJogo[linha_coluna[0]][linha_coluna[1]] = player;
+        
+        verificaJogo();
 
     }
 
 });
+
+// função para apenas zerar a matriz
+function iniciaMatriz(){
+    for (var i=0; i<3; i++){
+        matrizJogo['a'][i] = 0;
+    
+    }
+
+    for (var i=0; i<3; i++){
+        matrizJogo['b'][i] = 0;
+    
+    }
+
+    for (var i=0; i<3; i++){
+        matrizJogo['c'][i] = 0;
+    
+    }
+
+}
+
+// função que conta todas as linhas e colunas
+// refatorar função depois, dá pra ficar mais simples separando em partes
+function verificaJogo(){
+    var pontos = 0;
+
+    // horizontal
+    for (var i=0; i<3; i++){
+        pontos += matrizJogo['a'][i];
+
+    }
+    ganhador(pontos);
+
+	pontos = 0;
+    for (var i=0; i<3; i++){
+        pontos += matrizJogo['b'][i];
+
+    }
+    ganhador(pontos);
+
+	pontos = 0;
+    for (var i=0; i<3; i++){
+        pontos += matrizJogo['c'][i];
+
+    }
+    ganhador(pontos);
+
+    // vertical
+    for(var i=0; i<3; i++){
+        pontos = 0;
+        pontos += matrizJogo['a'][i];
+        pontos += matrizJogo['b'][i];
+        pontos += matrizJogo['c'][i];
+
+        ganhador(pontos);
+
+    }
+
+    // diagonal
+    pontos = 0;
+    pontos = matrizJogo['a'][0] + matrizJogo['b'][1] + matrizJogo['c'][2];
+    ganhador(pontos);
+
+    pontos = 0;
+    pontos = matrizJogo['a'][0] + matrizJogo['b'][1] + matrizJogo['c'][2];
+    ganhador(pontos);
+
+}
+
+// função que confere se realmente alguém ganhou
+function ganhador(pontos){
+    if(pontos === -3){
+        var jogada_1 = $('#entrada-p1').val();
+        alert(jogada_1 + ' venceu!');
+        $('.quadrado').off();
+    
+    } 
+    
+    else if(pontos === 3){
+        var jogada_2 = $('#entrada-p2').val();
+        alert(jogada_2 + ' venceu!');
+        $('.quadrado').off();
+    
+    }
+
+}
+
