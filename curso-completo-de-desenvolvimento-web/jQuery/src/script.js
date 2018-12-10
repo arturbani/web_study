3f6209020e41178f7aa57112f1167de0aeb131c1
Artur Bani <arturbani@gmail.com>
2018-12-10 10:07:00 -0200

    added click functions to the tictactoe

--- curso-completo-de-desenvolvimento-web/jQuery/src/script.js
+++ curso-completo-de-desenvolvimento-web/jQuery/src/script.js
@@ -1,3 +1,5 @@
+var rodada = 1;
+
 $(document).ready(function (){
     $('#btn-start').click( function (){ 
         // checagem dos nomes dos jogadores
@@ -27,9 +29,37 @@ $(document).ready(function (){
         $('#main-page').hide();
         $('#tic-tac-toe').show();
 
-        
-        
-
-
     });
+
+    $('.quadrado').click( function (){ 
+        var id_quadrado = this.id;
+        jogada(id_quadrado);
+    
+    });
+
+    function jogada(id){
+        var icon = '';
+        var player = 0;
+
+        // checa qual jogador clicou através da variavel rodada
+        // rodada % 2, se resta 1 é ímpar, jogador 1
+        // resta 0 é par, jogador 2
+        if ((rodada % 2) === 1){
+            icon = 'url("img/marcacao_1.png")';
+            player = -1;
+
+        }
+
+        else {
+            icon = 'url("img/marcacao_2.png")';
+            player = 1;
+
+        }
+
+        rodada++;
+
+        $('#'+id).css('background-image', icon);
+
+    }
+
 });
